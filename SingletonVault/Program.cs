cf4381b132f2b30379a09524f583dc7dec3c8627
Dominik Białowąs <s9325db@ms.wwsi.edu.pl>
2024-09-27 20:05:38 +0200

    Add project files.

---
+++ SingletonVault/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace SingletonVault
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var vault1 = Vault.Instance;
+            Console.WriteLine(vault1.getKey());
+
+            var valut2 = Vault.Instance;
+            Console.WriteLine(valut2.getKey());
+        }
+    }
+}
---
+++ SingletonVault/Vault.cs
@@ -0,0 +1,39 @@
+﻿namespace SingletonVault
+{
+    public sealed class Vault
+    {
+        private static Vault vault;
+        private static string key;
+        private static object syncRoot = new Object();
+        private Vault()
+        {
+            key = generateKey();
+        }
+
+        public static Vault Instance
+        {
+            get
+            {
+                lock (syncRoot)
+                {
+                    if (vault == null)
+                    {
+                        vault = new Vault();
+                    }
+                }
+
+                return vault;
+            }
+        }
+
+        public string getKey()
+        {
+            return key;
+        }
+
+        private string generateKey() 
+        {
+            return Guid.NewGuid().ToString();
+        }
+    }
+}
