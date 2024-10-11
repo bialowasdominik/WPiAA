c2c71670bb0be9f92d44c4904d3e25d6d1a2a421
Dominik Białowąs <s9325db@ms.wwsi.edu.pl>
2024-10-11 19:48:11 +0200

    Add RefusedBequest task

---
+++ RefusedBequest/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace RefusedBequest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    public interface IEmployee 
+    {
+        void Work();
+        void AttendMeeting();
+    }
+
+    public class Employee : IEmployee
+    {
+        public string Name { get; set; }
+        public string Position { get; set; }
+        public void Work() { Console.WriteLine("Working"); }
+        public void AttendMeeting() { Console.WriteLine("Attending meeting"); }
+    }
+
+    public class Manager : Employee
+    {
+        public void ManageTeam() { Console.WriteLine("Managing team"); }
+    }
+}
