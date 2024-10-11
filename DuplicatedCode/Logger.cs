13469ce54ed76936aae6e4c70b66064e31ab41b8
Dominik Białowąs <s9325db@ms.wwsi.edu.pl>
2024-10-11 18:45:01 +0200

    Add DuplicatedCode task

---
+++ DuplicatedCode/Logger.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DuplicatedCode
+{
+    public class Logger
+    {
+        public void LogError(string message, DateTime timestamp)
+        {
+            Console.WriteLine($"ERROR: {FormatLogMessage(message, timestamp)}");
+        }
+
+        public void LogWarning(string message, DateTime timestamp)
+        {
+            Console.WriteLine($"WARNING: {FormatLogMessage(message, timestamp)}");
+        }
+
+        private string FormatLogMessage(string message, DateTime timestamp) 
+        {
+            string formattedTimestamp = timestamp.ToString("yyyy-MM-dd HH:mm:ss");
+            string formattedMessage = $"[{formattedTimestamp}] {message}";
+            return formattedMessage;
+        }
+    }
+}
---
+++ DuplicatedCode/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace DuplicatedCode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger logger = new Logger();
+
+            logger.LogWarning("warrning message without duplicating code", DateTime.Now);
+            logger.LogError("error message without duplicating code", DateTime.Now);
+        }
+    }
+}
