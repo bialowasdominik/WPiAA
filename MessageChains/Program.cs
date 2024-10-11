b55f9545a98fc721b8c826854c8c4d90e77acd4e
Dominik Białowąs <s9325db@ms.wwsi.edu.pl>
2024-10-11 19:38:26 +0200

    Add MessageChains task

---
+++ MessageChains/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace MessageChains
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+
+            string cylinderSize = car.GetCylinder(); 
+            Console.WriteLine($"Cylider size: {cylinderSize}");
+        }
+    }
+    public class Car
+    {
+        private readonly Engine _engine = new();
+
+        public Engine GetEngine()
+        {   
+            return _engine;
+        }
+
+        public string GetCylinder() 
+        {
+            return _engine.GetCylinder().GetSize();
+        }
+    }
+
+    public class Engine
+    {
+        public Cylinder GetCylinder()
+        {
+            return new Cylinder();
+        }
+    }
+
+    public class Cylinder
+    {
+        public string GetSize()
+        {
+            return "2.0L";
+        }
+    }
+}
